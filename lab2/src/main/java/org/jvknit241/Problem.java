67058f937dfd069b07123bb6f19185fe97d9a4ab
Katik007 <kkabanovaa@list.ru>
2025-02-15 13:10:18 +0400

    (\)(\)

---
+++ lab2/src/main/java/org/jvknit241/Problem.java
@@ -0,0 +1,19 @@
+package org.jvknit241;
+
+class Problem {
+    private String description;
+    private int diff;
+
+    public Problem(String description, int diff) {
+        this.description = description;
+        this.diff = diff;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getDiff() {
+        return diff;
+    }
+}
